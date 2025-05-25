4079fa702fad119f63052f3b2d029963cc42fe55
Divakar <divakar@Divakars-MacBook-Air.local>
2025-05-25 19:42:55 +0530

    Added header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if (userData) {
+        dispatch(login({userData}));
+      } else {
+        dispatch(logout());
+      }
+    })
+    .finally(() => {setLoading(false)})
+  }, []);
   return (
     <>
       <h1>create app using app write </h1>
